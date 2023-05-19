8ea94f1c47beaab1029d2e61982f9c81b9f45c7e
IsaacFG1985 <105599356+IsaacFG1985@users.noreply.github.com>
2023-05-19 17:53:53 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,2 +1,2 @@
-loadstring(game.HttpGet('',true))()
+loadstring(game.HttpGet('https://raw.githubusercontent.com/IsaacFG1985/Roblox-lag-test-script/main/fps-test.lua',true))()
 loadstring(game.HttpGet('',true))()
